9d061f0f5a215e5a4da1edbf9a8407b74c1fdb47
Darthpaul0 <pablofr95@gmail.com>
2022-09-06 19:52:52 +0200

    App sin Context API

--- React/context/src/components/MyPage.js
+++ React/context/src/components/MyPage.js
@@ -1,7 +1,75 @@
-import React from "react";
+import { useState } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialTheme = "light";
+const initialLanguage = "es";
+
+const translations = {
+  es: {
+    headerTitle: "Mi aplicación SIN CONTEXT API",
+    headerSubtitle: "Mi Cabecera",
+    headerLight: "Claro",
+    headerDark: "Oscuro",
+    buttonLogin: "Iniciar sesión",
+    buttonLogout: "Cerrar sesión",
+    mainWelcome: "¡Bienvenido!",
+    mainHello: "Hola, Usuario",
+    mainContent: "Mi contenido principal",
+    footerTitle: "Mi pie de página",
+  },
+  en: {
+    headerTitle: "My app WITHOUT CONTEXT API",
+    headerSubtitle: "My Header",
+    headerLight: "Light",
+    headerDark: "Dark",
+    buttonLogin: "Login",
+    buttonLogout: "Logout",
+    mainWelcome: "Welcome!",
+    mainHello: "Hello, User",
+    mainContent: "Main content",
+    footerTitle: "My footer",
+  },
+};
 
 const MyPage = () => {
-  return <div><h2>Mi página</h2></div>;
+  const [theme, setTheme] = useState(initialTheme);
+  const [language, setLanguage] = useState(initialLanguage);
+  const [texts, setTexts] = useState(translations[language]);
+
+  // función para cambiar el tema de la página
+  const handleTheme = (e) => {
+    if (e.target.value === "light") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
+  // función para cambiar el lenguaje
+  const handleLanguage = (e) => {
+    if (e.target.value === "es") {
+      setLanguage("es");
+      setTexts(translations.es);
+    } else {
+      setLanguage("en");
+      setTexts(translations.en);
+    }
+  };
+
+  return (
+    <div className="my-page">
+      <Header
+        theme={theme}
+        handleTheme={handleTheme}
+        texts={texts}
+        handleLanguage={handleLanguage}
+      />
+      <Main theme={theme} texts={texts} />
+      <Footer theme={theme} texts={texts} />
+    </div>
+  );
 };
 
 export default MyPage;
