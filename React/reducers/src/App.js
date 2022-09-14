5f835d6e37b4d94575c3307e2e962dd98b29da43
Darthpaul0 <pablofr95@gmail.com>
2022-09-14 12:44:53 +0200

    CRUD API usando Reducers

--- React/reducers/src/App.js
+++ React/reducers/src/App.js
@@ -1,11 +1,14 @@
 import Contador from "./components/Contador";
 import ContadorMejorado from "./components/ContadorMejorado";
+import CrudAPI from "./components/CRUD API/CrudAPI";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <hr/>
+      <CrudAPI/>
       <hr />
       <ShoppingCart />
       <hr />
---
+++ React/reducers/src/components/CRUD API/CrudAPI.js
@@ -0,0 +1,173 @@
+import React, { useState, useEffect, useReducer } from "react";
+import { TYPES } from "../../actions/crudActions";
+import { helpHttp } from "../../helpers/helpHttp";
+import { crudInitialState, crudReducer } from "../../reducers/crudReducer";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
+
+const CrudAPI = () => {
+  //const [db, setDb] = useState(null);
+  const [state, dispatch] = useReducer(crudReducer, crudInitialState);
+  const { db } = state;
+
+  const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // variable que usaremos para llamar al helper
+  let api = helpHttp();
+  // endpoint
+  let url = `http://localhost:5000/jugadores`;
+
+  // usamos este useEffect para hacer la petición fetch
+  useEffect(() => {
+    // actualizamos la variable loading a true
+    setLoading(true);
+    helpHttp()
+      .get(url)
+      .then((res) => {
+        //console.log(res);
+        if (!res.err) {
+          //setDb(res);
+          dispatch({ type: TYPES.READ_ALL_DATA, payload: res });
+          // si no hay errores
+          setError(null);
+        } else {
+          //setDb(null);
+          dispatch({ type: TYPES.NO_DATA });
+          setError(res);
+        }
+        // volvemos a ponerlo en false una vez hemos cumplido la promesa
+        setLoading(false);
+      });
+  }, [url]);
+
+  // función para crear un registro - POST
+  const createData = (data) => {
+    // creamos un id único
+    data.id = Date.now();
+
+    /**
+     * Ejecutamos el método POST de api y le pasamos la url y los datos
+     * hay que especificar el tipo de datos que le vamos a pasar
+     */
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.post(url, options).then((res) => {
+      // console.log(res);
+      // si no hay errores, actualizamos la base de datos
+      if (!res.err) {
+        //setDb([...db, res]);
+        dispatch({ type: TYPES.CREATE_DATA, payload: res });
+      } else {
+        setError(res);
+      }
+    });
+
+    // asignamos el nuevo valor a db
+    //setDb([...db, data]);
+  };
+
+  // función para modificar un registro -- PUT
+  const updateData = (data) => {
+    // recuperamos el endpoint correspondiente al registro
+    let endpoint = `${url}/${data.id}`;
+
+    /**
+     * Para actualizar un registro usaremos PUT
+     */
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      // console.log(res);
+      // si no hay errores, actualizamos la base de datos
+      if (!res.err) {
+        // miramos en la base de datos si algún registro tiene esa id,
+        // y en ese caso reemplazamos la información
+        // let newData = db.map((el) => (el.id === data.id ? data : el));
+        //setDb(newData);
+        dispatch({ type: TYPES.UPDATE_DATA, payload: data });
+      } else {
+        setError(res);
+      }
+    });
+  };
+
+  // función para eliminar un registro -- DELETE
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `¿Seguro que quieres eliminar al jugador con id:'${id}?'`
+    );
+
+    if (isDelete) {
+      // recuperamos el endpoint correspondiente al registro
+      let endpoint = `${url}/${id}`;
+
+      /**
+       * Para eliminar un registro usaremos DELETE
+       */
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        // console.log(res);
+        // si no hay errores, actualizamos la base de datos
+        if (!res.err) {
+          // miramos en la base de datos si algún registro tiene esa id,
+          // y en ese caso lo borramos
+          // creamos un nuevo array con todos los id menos el eliminado
+          // let newData = db.filter((el) => el.id !== id);
+          //setDb(newData);
+          dispatch({ type: TYPES.DELETE_DATA, payload: id });
+        } else {
+          setError(res);
+        }
+      });
+    } else {
+      return;
+    }
+  };
+
+  // Renderizado de la APP
+  return (
+    <div>
+      <h2>CRUD API</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        {/* filtramos que la variable loading sea true */}
+        {loading && <Loader />}
+        {/* si hay un error, mostramos el mensaje de error */}
+        {error && (
+          <Message
+            msg={`Error ${error.status}: ${error.statusText}`}
+            bgColor="red"
+          />
+        )}
+        {/* filtramos que la bbdd no sea null */}
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
+      </article>
+    </div>
+  );
+};
+
+export default CrudAPI;
