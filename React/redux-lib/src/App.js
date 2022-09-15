e5ac23b36394f3690087cf3564e55eeb0bfd8c55
Darthpaul0 <pablofr95@gmail.com>
2022-09-15 13:23:27 +0200

    Intro to Redux

---
+++ React/redux-lib/src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import TeoriaRedux from "./components/TeoriaRedux";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Redux</h1>
+        <hr />
+        <TeoriaRedux />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
